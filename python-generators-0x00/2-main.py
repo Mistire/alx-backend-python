e382de80c684fbcca2510a2b07dab224dd1f0947
Mistire <mdwithgod@gmail.com>
2025-05-18 22:33:12 +0300

    Hotfix

--- python-generators-0x00/2-main.py
+++ python-generators-0x00/2-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
 
+import sys
 processing = __import__('1-batch_processing')
 
-##### print processed users in a batch of 50
 try:
-    processing.batch_processing(50)
+    for user in processing.batch_processing(50):
+        print(user)
 except BrokenPipeError:
     sys.stderr.close()
