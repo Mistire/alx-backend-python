f55b380ede2fc08c3799ec7294328c3e546261ec
Mistire <mdwithgod@gmail.com>
2025-05-31 11:11:34 +0300

    fix: Solve the error that is making the test to fail

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import unittest
 from parameterized import parameterized
@@ -7,7 +7,7 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
   @parameterized.expand([
     ({"a": 1}, ["a"], 1),
-    ({"a": {"b": 2}}, ["a"], 2),
+    ({"a": {"b": 2}}, ["a"], {"b": 2}),
     ({"a": {"b": 2}}, ["a", "b"], 2),
   ])
   def test_access_nested_map(self, nested_map, path, expected):
