3871429522d2036e5468d5c714e71835f3acb2f3
Mistire <mdwithgod@gmail.com>
2025-05-31 12:11:34 +0300

    Add a docstring to describe the class and function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 import unittest
+from typing import Dict, List, Any
 from parameterized import parameterized
 from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
+  """Unit tests for the `access_nested_map` function from the `utils` module."""
+  
   @parameterized.expand([
     ({"a": 1}, ["a"], 1),
     ({"a": {"b": 2}}, ["a"], {"b": 2}),
     ({"a": {"b": 2}}, ["a", "b"], 2),
   ])
-  def test_access_nested_map(self, nested_map, path, expected):
+  def test_access_nested_map(self, nested_map: Dict[str, Any], path: List[str], expected):
+    """
+    Test that `access_nested_map` returns the correct value when given valid nested maps and path. 
+    """
     self.assertEqual(access_nested_map(nested_map, path), expected) 
 
 
@@ -18,9 +24,12 @@ class TestAccessNestedMap(unittest.TestCase):
     ({}, ["a"]),
     ({"a": 1}, ["a", "b"])
   ])
-  def test_access_nested_map_exception(self, nested_map, key):
+  def test_access_nested_map_exception(self, nested_map: Dict[str, Any], path: List[str]) -> None:
+    """
+    Test that `access_nested_map` raises a KeyError when the path doesn't exist in the nested map.
+    """
     with self.assertRaises(KeyError):
-      access_nested_map(nested_map, key)
+      access_nested_map(nested_map, path)
 
 if '__main__' == __name__:
   unittest.main()
