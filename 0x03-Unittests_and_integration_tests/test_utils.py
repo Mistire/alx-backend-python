9fc19d886a93526930aa2e3895f54ed92592ffe9
Mistire <mdwithgod@gmail.com>
2025-05-31 15:54:14 +0300

    Hot fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import unittest
 from typing import Dict, List, Any
@@ -35,19 +35,21 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
   """Unit test for the `get_json` function in `utils`"""
 
-  @parameterized.expand([
-    ("http://example.com", {"payload": True}),
-    ("http://holberton.io", {"payload": False}),
-  ])
-  def test_get_json(self, test_url, test_payload):
-    """Test that `get_json` returns expected JSON response"""
-    mock_response = Mock()
-    mock_response.json.return_value = test_payload
-    with patch('utils.requests.get', return_value=test_payload) as mock_get:
-      result = get_json(test_url)
+  def test_get_json(self):
+    test_cases = [
+      ("http://example.com", {"payload": True}),
+      ("http://holberton.io", {"payload": False}),
+    ]
 
-      self.assertEqual(result, test_payload)
-      mock_get.assert_called_once_with(test_url)
+    for test_url, test_payload in test_cases:
+      
+      mock_response = Mock()
+      mock_response.json.return_value = test_payload
+      with patch('utils.requests.get', return_value=test_payload) as mock_get:
+        result = get_json(test_url)
+
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
 
 
 
