8447e9fb1065f78da255fefc895f8ffec759d6c9
Mistire <mdwithgod@gmail.com>
2025-05-31 15:34:13 +0300

    Create a mock test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -33,15 +33,16 @@ class TestAccessNestedMap(unittest.TestCase):
       access_nested_map(nested_map, path)
 
 class TestGetJson(unittest.TestCase):
+  """Unit test for the `get_json` function in `utils`"""
 
   @parameterized.expand([
     ("http://example.com", {"payload": True}),
     ("http://holberton.io", {"payload": False}),
   ])
   def test_get_json(self, test_url, test_payload):
+    """Test that `get_json` returns expected JSON response"""
     mock_response = Mock()
     mock_response.json.return_value = test_payload
-
     with patch('utils.requests.get', return_value=test_payload) as mock_get:
       result = get_json(test_url)
 
