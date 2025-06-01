c2da4caeb7f7c2cef12f1c28fefe3597393ca39f
Mistire <mdwithgod@gmail.com>
2025-06-01 06:51:06 +0300

    Fix: Update chats app model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -4,10 +4,12 @@ import uuid
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=30)  
+    last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']  # Keep username if AbstractUser requires it
+    REQUIRED_FIELDS = ['username']  
 
     def __str__(self):
         return self.email
