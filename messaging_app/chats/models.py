fa7fc316176b0e7a1f3106f6bc80653811dd72e2
Mistire <mdwithgod@gmail.com>
2025-06-01 06:36:04 +0300

    Create data models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+  pass
+
+class Conversation(models.Model):
+  users =  models.ManyToManyField('User', related_name='conversation')
+  created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+  sender = models.ForeignKey('User', on_delete=models.CASCADE)
+  Conversation = models.ForeignKey('conversation', on_delete=models.CASCADE, realted_name='messages')
+  content = models.TextField()
+  timestamp = models.DateTimeField(auto_now_add=True)
