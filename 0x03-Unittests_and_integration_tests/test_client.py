3767d1561f00c6d6ce372d09179bc7f857d17997
Mistire <mdwithgod@gmail.com>
2025-06-07 17:20:52 +0300

    Implement a unit test GithubOrgClient.org

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+class TestGithubOrrgClient(unittest.TestCase):
+
+  @parameterized.expand([
+    ("google",),
+    ("abc",)
+  ])
+  @patch('client.get_json')
+  def test_org(self, org_name, mock_get_json):
+    """Test GIthubOrgClient.org return expected result"""
+    expected = {"login": org_name}
+    mock_get_json.return_value = expected
+
+    client = GithubOrgClient(org_name)
+    self.assertEqual(client.org, expected)
+    mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+
+
+if '__main__' == __name__:
+  unittest.main()
