1fc89dc59983b58fd739e6105b4f4422545c08aa
Mistire <mdwithgod@gmail.com>
2025-06-08 13:02:00 +0300

    fix: include default router for the url

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter  # ✅ imported
+from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
