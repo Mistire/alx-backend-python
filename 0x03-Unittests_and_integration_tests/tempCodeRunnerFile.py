91be0490a26b06ccac995e95dce073ad60aae7f7
Mistire <mdwithgod@gmail.com>
2025-06-08 08:41:55 +0300

    Create the TestIntegrationGithubOrgClient(unittest.TestCase) class and implement the setUpClass and tearDownClass which are part of the unittest.TestCase API.

--- 0x03-Unittests_and_integration_tests/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def my_side_effect(url):
-            """ Side Effect function for test """
-            test_url = "https://api.github.com/orgs/google"
-            if url == test_url:
-                return cls.org_payload
-            return cls.repos_payload
