117fb58f08e1402ee58d15c94086b49ff4e8d0d9
Mistire <mdiwithgod@gmail.com>
2025-07-17 14:21:57 +0300

    fix: Update the code so it would follow pycodestyle..2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,7 @@ import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
-
-
+"""Unit tests for utils.py functions."""
 class TestAccessNestedMap(unittest.TestCase):
     """A class to test access_nested_map function"""
 
