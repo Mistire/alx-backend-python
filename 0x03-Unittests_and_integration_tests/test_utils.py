3fb1fbc530b9436f12da0cda25c2e6fe41ce82e6
Mistire <mdiwithgod@gmail.com>
2025-07-17 09:30:03 +0300

    Implement the TestGetJson.test_get_json method to test that utils.get_json returns the expected result.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import unittest
+
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
   """A class to test access_nested_map function"""
@@ -24,6 +26,34 @@ class TestAccessNestedMap(unittest.TestCase):
     """A function to test access_nested_map function"""
     with self.assertRaises(KeyError):
       access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+  """A class to test get_json function"""
+
+  @patch('utils.requests.get')
+  def test_get_json(self, mock_get):
+    """A function to test get_json function"""
+    def mock_get_side_effect(url):
+      mock_response = Mock()
+      if url == "http://example.com":
+        mock_response.json.return_value = {"payload": "True"}
+      elif url == "http://holberton.io":
+        mock_response.json.return_value = {"payload": "False"}
+      else:
+        mock_response.json.return_value = {"error": "not found"}
+      return mock_response
+    
+    mock_get.side_effect = mock_get_side_effect
+
+    result1 = get_json("http://example.com")
+    self.assertEqual(result1, {"payload": "True"})
+    result2 = get_json("http://holberton.io")
+    self.assertEqual(result2, {"payload": "False"})
+
+    self.assertEqual(mock_get.call_count, 2)
+    mock_get.assert_any_call("http://example.com")
+    mock_get.assert_any_call("http://holberton.io")
+
 
 
 if __name__ == '__main__':
