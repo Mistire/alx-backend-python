adc41427fe7edbd37554e8bb2fa99c5185505714
Mistire <mdiwithgod@gmail.com>
2025-07-20 13:02:21 +0300

    fix: fix the custum user auth model

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from chats.views import UserViewSet, MessageViewSet, ConversationViewSet
+from .views import MessageViewSet, ConversationViewSet
 
 router = routers.DefaultRouter()
-router.register(r"users", UserViewSet, basename="user")
-# router.register(r"messages", MessageViewSet, basename="message")
+# router.register(r"users", UserViewSet, basename="user")
+router.register(r"messages", MessageViewSet, basename="message")
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 
 conversations_router = routers.NestedDefaultRouter(router, r"conversations", lookup="conversation")
