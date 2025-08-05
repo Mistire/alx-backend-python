598f8095e0ac0092206df233957118b194f2fa1b
Mistire <mdiwithgod@gmail.com>
2025-08-05 16:40:18 +0300

    fix: Add crud operation in to poermission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -7,19 +7,32 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+    
 
 class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Allows access only to participants of the conversation.
+    Custom permission:
+    - Allow only authenticated users.
+    - Allow only participants of the conversation to GET, PUT, PATCH, DELETE.
     """
-
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        if hasattr(obj, 'participants'):
-            return user in obj.participants.all()
-        elif hasattr(obj, 'conversation'):
-            return user in obj.conversation.participants.all()
-        return False
 
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        method = request.method
+
+
+        if hasattr(obj, 'participants'):
+            is_participant = user in obj.participants.all()
+        elif hasattr(obj, 'conversation'):
+            is_participant = user in obj.conversation.participants.all()
+        else:
+            return False
+
+        if method in ['GET', 'PUT', 'PATCH', 'DELETE']:
+            return is_participant
+
+      
+        return True
