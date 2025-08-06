ddbdf59054a8763ee794e0ef70ad50a52987e4b5
Mistire <mdiwithgod@gmail.com>
2025-08-06 19:06:45 +0300

    Create a middleware class RestrictAccessByTimeMiddleware with two methods, __init__and__call__. that check the current server time and deny access by returning an error 403 Forbidden

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,24 +1,38 @@
-# chats/middleware.py
-
 import logging
-from datetime import datetime
-
-
-logger = logging.getLogger('request_logger')
-handler = logging.FileHandler('requests.log') 
-formatter = logging.Formatter('%(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+from datetime import datetime, time
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.logger = logging.getLogger("request_logger")
+        handler = logging.FileHandler("requests.log")
+        formatter = logging.Formatter('%(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
 
     def __call__(self, request):
-        user = request.user if request.user.is_authenticated else 'Anonymous'
+        user = request.user if request.user.is_authenticated else "Anonymous"
         log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
-        logger.info(log_message)
+        self.logger.info(log_message)
 
         response = self.get_response(request)
         return response
+    
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        now = datetime.now().time()
+        allowed_start = time(18, 0)  
+        allowed_end = time(21, 0)   
+
+        if request.path.startswith('/api/messages/') or request.path.startswith('/chats'):
+            if not (allowed_start <= now <= allowed_end):
+                return HttpResponseForbidden("Chat access is only allowed between 6PM and 9PM.")
+
+        return self.get_response(request)
+
